c79e0ee09d828a5206e0298cf0481fd388a5503e
dimuthu Pramuditha <dimuthupramuditha2@gmail.com>
2025-08-25 02:20:34 +0530

    feat: add welcome page with library system overview and navigation to login

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "libraryMS";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link href="pages/index.css" rel="stylesheet">
+    <title>Welcome Page</title>
+</head>
+<body>
+<div>
+    <div class="overflow-hidden bg-white pt-1">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+                <div class="lg:pt-4 lg:pr-8">
+                    <div class="lg:max-w-lg">
+                        <h2 class="text-base/7 font-semibold text-indigo-600">Borrowing faster</h2>
+                        <p class="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">XYZ Library System</p>
+                        <p class="mt-6 text-lg/8 text-gray-700">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</p>
+                        <dl class="mt-10 max-w-xl space-y-8 text-base/7 text-gray-600 lg:max-w-none">
+                            <div class="relative pl-9">
+                                <dt class="inline font-semibold text-gray-900">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 absolute top-1 left-1 text-indigo-600">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
+                                    </svg>
+                                    Borrowing Books
+                                </dt>
+                                <dd class="inline">Simply browse available titles, click to borrow, and start your reading journey. Keep track of your borrowed books and due dates all in one place!</dd>
+                            </div>
+                            <div class="relative pl-9">
+                                <dt class="inline font-semibold text-gray-900">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 absolute top-1 left-1 text-indigo-600">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                                    </svg>
+
+                                    Search Books
+                                </dt>
+                                <dd class="inline">Search by title, author, or ISBN to quickly find exactly what you're looking for. Filter through our comprehensive collection and find your next great read! </dd>
+                            </div>
+                            <div class="relative pl-9">
+                                <dt class="inline font-semibold text-gray-900">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 absolute top-1 left-1 text-indigo-600">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                    </svg>
+
+                                    Update Profile
+                                </dt>
+                                <dd class="inline">Update your contact details, preferences, and manage your library settings. Your profile helps us provide you with the best library experience! </dd>
+                            </div>
+                        </dl>
+                    </div>
+                    <div>
+                        <a href="login.php" class="mt-10 inline-block rounded-md bg-indigo-600 px-3.5 py-2.5 text-base/7 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 hover:scale-95 transition-transform duration-150">
+                            Get started
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 ml-2 inline-block">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+                <img src="pages/assets/welcome02.png" alt="" class="w-3xl max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-228 md:-ml-4 lg:-ml-0 mt-5" />
+            </div>
+        </div>
+    </div>
+
+
+</div>
+</body>
+</html>
