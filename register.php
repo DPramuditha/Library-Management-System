532bb2228de12a5baa45e86255b2241c85db7d4f
dimuthu Pramuditha <dimuthupramuditha2@gmail.com>
2025-08-28 01:16:58 +0530

    feat: enhance login and registration buttons with icons and adjust styling

--- register.php
+++ register.php
@@ -179,7 +179,11 @@ $conn->close();
             </div>
 
             <div>
-                <button type="submit" class="w-full flex justify-center rounded-md bg-indigo-600 p-4 text-sm font-semibold text-white shadow-xs hover:bg-blue-600 focus-visible:outline focus-visible:outline-indigo-600 hover:scale-95 transform duration-300">Register</button>
+
+                <button type="submit" class="w-full flex justify-center rounded-md bg-indigo-600 p-3 text-sm font-semibold text-white shadow-xs hover:bg-blue-600 focus-visible:outline focus-visible:outline-indigo-600 hover:scale-95 transform duration-300">
+                    <svg class="mr-2" fill="none" height="24" stroke-width="1.5" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M17 10H20M23 10H20M20 10V7M20 10V13" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/><path d="M1 20V19C1 15.134 4.13401 12 8 12V12C11.866 12 15 15.134 15 19V20" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/><path d="M8 12C10.2091 12 12 10.2091 12 8C12 5.79086 10.2091 4 8 4C5.79086 4 4 5.79086 4 8C4 10.2091 5.79086 12 8 12Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/></svg>
+                    Register
+                </button>
             </div>
         </form>
 
